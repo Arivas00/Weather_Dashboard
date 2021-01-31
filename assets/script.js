f555002f3a7c74db34b68b66e2946af418c67bdb
Arivas <33260080+Arivas00@users.noreply.github.com>
2021-01-31 17:33:18 -0500

    Weather API enabled in script

--- assets/script.js
+++ assets/script.js
@@ -3,14 +3,22 @@ $(document).ready(function () {
     var DateTime = luxon.DateTime;
     var date = DateTime.local().setLocale('en').toLocaleString();
 
-    $("#search").on('click', function() {
+    $("#search").on('click', function () {
 
-    var city = $("#citysearch").val();
-    var currentWeather = "api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIkey;
-    // var oneAPI = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude={part}&appid=" + APIkey;
-        
+        var city = $("#citysearch").val();
+        var APIkey = "ec15295cc65f712aedab78c75dd81b07"
+        var currentWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIkey;
+        // var oneAPI = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude={part}&appid=" + APIkey;
+
         $("#day1").text(date)
-        
+
+        fetch(currentWeather)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data);
+            })
 
 
 
