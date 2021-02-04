0b5a7029c42c6069833d5a6f22e71f3e8f9df64d
Arivas <33260080+Arivas00@users.noreply.github.com>
2021-02-03 22:32:02 -0500

    Added hiden displays and updated readme

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # Weather_Dashboard
+
+## Developer Notes
+
+As a developer I was tasked with taking a third-party API's data to implenet in an application to display weather for multiple cities.
+
+## Criteria
+
+```
+WHEN opening the application the user is displayed a weather dashboard with form inputs
+WHEN the user searches for a city
+THEN the user is presented with current and future conditions for that city and that city is added to the search history
+WHEN the user views current weather conditions for that city
+THEN the user is presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+WHEN the user views the UV index
+THEN user is presented with a color that indicates whether the conditions are favorable, moderate, or severe
+WHEN user views future weather conditions for that city
+THEN the user is presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
+WHEN the user clicks on a city in the search history
+THEN the user is again presented with current and future conditions for that city
+```
+
+## Mock-Up
+
+The following image shows the web application's appearance and functionality:
+
+![The weather app includes a search option, a list of cities, and a five-day forecast and current weather conditions for Atlanta.](./Assets/06-server-side-apis-homework-demo.png)
+
+## Deployment
+
+https://arivas00.github.io/Weather_Dashboard/
--- assets/script.js
+++ assets/script.js
@@ -3,16 +3,27 @@ $(document).ready(function () {
     var DateTime = luxon.DateTime;
     var now = DateTime.local();
 
+    var city = $("#citysearch").val();
+    var APIkey = "&appid=ec15295cc65f712aedab78c75dd81b07"
+    var imperial = "&units=imperial"
+    var currentWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + city
+    var oneAPI = "https://api.openweathermap.org/data/2.5/onecall?lat="
+
+
     $("#search").on('click', function () {
+        var key = $("#citysearch")
+        var searches = localStorage.getItem(key);
+        var row = $("<div>").addClass("row")
+        var card = $("<div>").addClass("card")
+        var cardBody = $("<div>").addClass("card-body btn")
+        localStorage.setItem(key, city);
+        $("#aside").append(row.append(card.append(cardBody.append(searches))))
 
-        var city = $("#citysearch").val();
-        var APIkey = "&appid=ec15295cc65f712aedab78c75dd81b07"
-        var imperial = "&units=imperial"
-        var currentWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + city
-        var oneAPI = "https://api.openweathermap.org/data/2.5/onecall?lat="
-        var fiveDay = "https://api.openweathermap.org/data/2.5/forecast?q=" + city
+        $("h2").removeClass("visually-hidden")
+        $("div").removeClass("visually-hidden")
+        $("p").removeClass("visually-hidden")
 
-        $("#day1").text(now.toLocaleString());
+        $("#day1").text(city + " " + now.toLocaleString());
         $("#day2").text(now.plus({ days: 1 }).toLocaleString());
         $("#day3").text(now.plus({ days: 2 }).toLocaleString());
         $("#day4").text(now.plus({ days: 3 }).toLocaleString());
@@ -34,7 +45,17 @@ $(document).ready(function () {
                     })
                     .then(function (data) {
                         console.log(data);
-                        $("#uv").text("UV Index: " + data.current.uvi)
+
+                        if (data.current.uvi < 2) {
+                            $("#uvNum").addClass("bg-success")
+                            $("#uvNum").text(data.current.uvi)
+                        } else if (data.current.uvi < 7 && data.current.uvi > 2) {
+                            $("#uvNum").addClass("bg-warning")
+                            $("#uvNum").text(data.current.uvi)
+                        } else {
+                            $("#uvNum").addClass("bg-danger")
+                            $("#uvNum").text(data.current.uvi)
+                        }
 
                         $("#temp2").text("Temp: " + data.daily[1].temp.day + " °F");
                         $("#temp3").text("Temp: " + data.daily[2].temp.day + " °F");
@@ -49,20 +70,25 @@ $(document).ready(function () {
                         $("#humid5").text("Humidity: " + data.daily[4].humidity + "%");
                         $("#humid6").text("Humidity: " + data.daily[5].humidity + "%");
 
-                        
+                        $("#emoji1").attr("src", "http://openweathermap.org/img/wn/" + data.daily[0].weather[0].icon + ".png")
                         $("#emoji2").attr("src", "http://openweathermap.org/img/wn/" + data.daily[1].weather[0].icon + ".png");
                         $("#emoji3").attr("src", "http://openweathermap.org/img/wn/" + data.daily[2].weather[0].icon + ".png");
                         $("#emoji4").attr("src", "http://openweathermap.org/img/wn/" + data.daily[3].weather[0].icon + ".png");
                         $("#emoji5").attr("src", "http://openweathermap.org/img/wn/" + data.daily[4].weather[0].icon + ".png");
                         $("#emoji6").attr("src", "http://openweathermap.org/img/wn/" + data.daily[5].weather[0].icon + ".png");
-                    
+
                     })
 
                 $("#temp1").text("Temperature: " + data.main.temp + " °F");
                 $("#wind").text("Wind Speed: " + data.wind.speed + " MPH")
 
+
+
             })
+        // Set Local storage
+
 
     })
+
 
 })
